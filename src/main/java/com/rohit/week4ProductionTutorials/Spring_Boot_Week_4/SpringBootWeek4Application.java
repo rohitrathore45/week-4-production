0ba9377428ecd3c3044afe0d288d028f6a1006fb
Rohit Rathore <rohitjeevanrathore@gmail.com>
2025-09-21 14:20:31 +0530

    Initailized the project

---
+++ src/main/java/com/rohit/week4ProductionTutorials/Spring_Boot_Week_4/SpringBootWeek4Application.java
@@ -0,0 +1,13 @@
+package com.rohit.week4ProductionTutorials.Spring_Boot_Week_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeek4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeek4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohit/week4ProductionTutorials/Spring_Boot_Week_4/SpringBootWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.week4ProductionTutorials.Spring_Boot_Week_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeek4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
